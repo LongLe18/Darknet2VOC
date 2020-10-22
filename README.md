61ecb2c195d7606b670b5b035f9502521a4f7cb7
Lê Ngọc Long <53988728+LongLe18@users.noreply.github.com>
2020-10-22 22:04:42 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+################# USAGE ##################
+1. change ANNOTATIONS_DIR_PREFIX (line 30 in txt-to-VOC.py): included image and txt (darknet).
+2. create folder "converted_labels".
+3. create folder "images" and copy all images in "ANNOTATIONS_DIR_PREFIX" save there.
+4. run "txt-to-VOC.py"
---
+++ txt-to-VOC.py
@@ -0,0 +1,120 @@
+# Script to convert yolo annotations to voc format
+
+# Sample format
+# <annotation>
+#     <folder>_image_fashion</folder>
+#     <filename>brooke-cagle-39574.jpg</filename>
+#     <size>
+#         <width>1200</width>
+#         <height>800</height>
+#         <depth>3</depth>
+#     </size>
+#     <segmented>0</segmented>
+#     <object>
+#         <name>head</name>
+#         <pose>Unspecified</pose>
+#         <truncated>0</truncated>
+#         <difficult>0</difficult>
+#         <bndbox>
+#             <xmin>549</xmin>
+#             <ymin>251</ymin>
+#             <xmax>625</xmax>
+#             <ymax>335</ymax>
+#         </bndbox>
+#     </object>
+# <annotation>
+import os
+import xml.etree.cElementTree as ET
+from PIL import Image
+
+ANNOTATIONS_DIR_PREFIX = "a"
+
+DESTINATION_DIR = "converted_labels"
+
+CLASS_MAPPING = {
+    '0': 'drone'
+    # Add your remaining classes here.
+}
+
+
+def create_root(file_prefix, file_sulfix, width, height):
+    root = ET.Element("annotations")
+    ET.SubElement(root, "filename").text = "{}.{}".format(file_prefix, file_sulfix)
+    ET.SubElement(root, "folder").text = "images"
+    size = ET.SubElement(root, "size")
+    ET.SubElement(size, "width").text = str(width)
+    ET.SubElement(size, "height").text = str(height)
+    ET.SubElement(size, "depth").text = "3"
+    return root
+
+
+def create_object_annotation(root, voc_labels):
+    for voc_label in voc_labels:
+        obj = ET.SubElement(root, "object")
+        ET.SubElement(obj, "name").text = voc_label[0]
+        ET.SubElement(obj, "pose").text = "Unspecified"
+        ET.SubElement(obj, "truncated").text = str(0)
+        ET.SubElement(obj, "difficult").text = str(0)
+        bbox = ET.SubElement(obj, "bndbox")
+        ET.SubElement(bbox, "xmin").text = str(voc_label[1])
+        ET.SubElement(bbox, "ymin").text = str(voc_label[2])
+        ET.SubElement(bbox, "xmax").text = str(voc_label[3])
+        ET.SubElement(bbox, "ymax").text = str(voc_label[4])
+    return root
+
+
+def create_file(file_prefix, file_sulfix, width, height, voc_labels):
+    root = create_root(file_prefix, file_sulfix, width, height)
+    root = create_object_annotation(root, voc_labels)
+    tree = ET.ElementTree(root)
+    tree.write("{}/{}.xml".format(DESTINATION_DIR, file_prefix))
+
+
+def read_file(file_path):
+    file = file_path.split("\\")
+    file_prefix = file[1].split(".txt")[0]
+    try:
+        image_file_name = "{}.jpg".format(file_prefix)
+        file_sulfix = "jpg"
+        img = Image.open("{}/{}".format("images", image_file_name))
+    except FileNotFoundError:
+        print("Error")
+        image_file_name = "{}.JPEG".format(file_prefix)
+        file_sulfix = "JPEG"
+        img = Image.open("{}/{}".format("images", image_file_name))
+    w, h = img.size
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        voc_labels = []
+        for line in lines:
+            voc = []
+            line = line.strip()
+            data = line.split()
+            voc.append(CLASS_MAPPING.get(data[0]))
+            bbox_width = float(data[3]) * w
+            bbox_height = float(data[4]) * h
+            center_x = float(data[1]) * w
+            center_y = float(data[2]) * h
+            voc.append(center_x - (bbox_width / 2))
+            voc.append(center_y - (bbox_height / 2))
+            voc.append(center_x + (bbox_width / 2))
+            voc.append(center_y + (bbox_height / 2))
+            voc_labels.append(voc)
+        create_file(file_prefix, file_sulfix, w, h, voc_labels)
+    print("Processing complete for file: {}".format(file_path))
+
+
+def start():
+    if not os.path.exists(DESTINATION_DIR):
+        os.makedirs(DESTINATION_DIR)
+    for (dirpath, dirnames, filenames) in os.walk(ANNOTATIONS_DIR_PREFIX):
+        for filename in filenames:
+            if filename.endswith('.txt'):
+                print(os.path.join(dirpath, filename))                
+                read_file(os.path.join(dirpath, filename))
+            else:
+                print("Skipping file: {}".format(filename))
+
+
+if __name__ == "__main__":
+    start()
